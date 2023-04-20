0a8bdd3fe06d64d04dabf6e19af3e129cdc1cc96
Angel <angel-ortega27@hotmail.com>
2023-04-19 17:35:47 -0700

    added helper function to grab id's added default mode

--- etch-a-sketch-logic.js
+++ etch-a-sketch-logic.js
@@ -1,64 +1,84 @@
-let btn = document.getElementById("user")
-    btn.addEventListener('click', createGrid)
+//Helper function to grab document ID's without repeating code
+const $ = (id) => {
+  return document.getElementById(id);
+};
+
+let startBtn = $("user");
+startBtn.addEventListener("click", createGrid);
+
+let deleteBtn = $("delete");
+deleteBtn.addEventListener("click", clearGrid);
+
+let defaultBtn = $("default");
+defaultBtn.addEventListener("click", defaultMode);
+
+let rainbowBtn = $("rainbow");
+rainbowBtn.addEventListener("click", rainbowMode);
+
+let ereaseBtn = $("eraser");
+ereaseBtn.addEventListener("click", eraserBox);
 
 function createGrid() {
-    let container = document.getElementById("container");
+  let container = document.getElementById("container");
 
-    let rows = prompt("How many rows do you want?: ");
-    if (rows > 100) {
-        alert('Please pick a number less than 100');
-        return;
-    }
-    let i = 0;
-    let x = rows * rows;
+  let rows = prompt("How many rows do you want?: ");
+  if (rows > 100) {
+    alert("Please pick a number less than 100");
+    return;
+  }
+  let i = 0;
+  let x = rows * rows;
 
-    document.documentElement.style.setProperty("--columns-row", rows);
+  document.documentElement.style.setProperty("--columns-row", rows);
 
-    for (i = 0; i < x; i++) {
-        const div = document.createElement("div");
-        div.className = 'box';
-        document.getElementById("container").appendChild(div);
-        div.addEventListener("mouseenter", function() {
-        this.style.backgroundColor = "black";
-        });
-    }
+  for (i = 0; i < x; i++) {
+    const div = document.createElement("div");
+    div.className = "box";
+    document.getElementById("container").appendChild(div);
+    div.addEventListener("mouseenter", function () {
+      this.style.backgroundColor = "black";
+    });
+  }
 }
-let btn2 = document.getElementById("delete");
-    btn2.addEventListener('click', clearGrid);
 
 function clearGrid() {
-let element = document.querySelectorAll('.box');
-element.forEach(box => {
-    box.remove();
-});
+  let element = document.querySelectorAll(".box");
+  element.forEach((box) => {
+    box.style.backgroundColor = "ghostwhite";
+  });
 }
-let btn3 = document.getElementById("rainbow");
-    btn3.addEventListener('click', rainbowMode)
 
-function rainbowMode () {
-    let updated = document.querySelectorAll('.box');
-   
-    updated.forEach(box => {
-        box.addEventListener("mouseenter", () => {
-        box.style.backgroundColor = getRandomColor();
+function rainbowMode() {
+  let updated = document.querySelectorAll(".box");
+
+  updated.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.style.backgroundColor = getRandomColor();
     });
- });
-}
-function getRandomColor() {
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
-let boxColor = "#" + randomColor;
-return boxColor;
+  });
 }
 
-let btn4 = document.getElementById("eraser");
-btn4.addEventListener('click', eraserBox)
+function getRandomColor() {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let boxColor = "#" + randomColor;
+  return boxColor;
+}
 
 function eraserBox() {
-    let erased = document.querySelectorAll('.box') 
+  let erased = document.querySelectorAll(".box");
 
-    erased.forEach(box =>{
-        box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = "ghostwhite";
-        });
+  erased.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.style.backgroundColor = "ghostwhite";
     });
+  });
+}
+function defaultMode() {
+  let box = document.querySelectorAll(".box");
+
+  box.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.style.backgroundColor = "black";
+    });
+  });
 }
